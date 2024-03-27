c6b6d784e3d72b00f21f1f47770f63f22015bd5b
Nazar <nnakapeliukh@gmail.com>
2024-03-27 01:13:38 -0400

    Saves changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,19 +1,32 @@
+async function searchJSON() {
+  const searchInput = document.getElementById("searchInput");
 
+  const searchTerm = searchInput.value.toLocaleLowerCase().slice(0, 5);
 
+  let searchResults;
 
+  const data = await (await fetch("./travel_recommendation_api.json")).json();
+  console.log(data);
 
+  switch (searchTerm) {
+    case "beach":
+      searchResults = data.beaches;
+    case "templ":
+      searchResults = data.temples;
+      break;
+    case "count":
+      searchResults = data.countries;
+      break;
+    default:
+      break;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-const data = await fetch("./travel_recommendation_api.json");
-console.log(data);
+    if (searchResults) {
+        const searchResultContainer = document.getElementById("search-results");
+        searchResults.forEach(place => {
+            
+        });
+    }
+  
+    console.log(searchResults);
+}
