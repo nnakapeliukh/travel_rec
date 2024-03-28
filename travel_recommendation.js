9a7972048adc579bf157dcb191168d5692c9ab9e
Nazar <nnakapeliukh@gmail.com>
2024-03-27 22:30:34 -0400

    Adds travel recomendations

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,32 +1,58 @@
 async function searchJSON() {
-  const searchInput = document.getElementById("searchInput");
+    const searchInput = document.getElementById("searchInput");
 
-  const searchTerm = searchInput.value.toLocaleLowerCase().slice(0, 5);
+    const searchTerm = searchInput.value.toLocaleLowerCase().slice(0, 5);
 
-  let searchResults;
+    let searchResults;
 
-  const data = await (await fetch("./travel_recommendation_api.json")).json();
-  console.log(data);
+    const data = await (await fetch("./travel_recommendation_api.json")).json();
+    switch (searchTerm) {
+        case "beach":
+            searchResults = data.beaches;
+            break;
+        case "templ":
+            searchResults = data.temples;
+            break;
+        case "count":
+            searchResults = data.countries;
+            break;
+        default:
+            break;
+    }
 
-  switch (searchTerm) {
-    case "beach":
-      searchResults = data.beaches;
-    case "templ":
-      searchResults = data.temples;
-      break;
-    case "count":
-      searchResults = data.countries;
-      break;
-    default:
-      break;
-  }
-
-    if (searchResults) {
+    if (searchResults && searchTerm !== "count") {
         const searchResultContainer = document.getElementById("search-results");
+        searchResultContainer.innerHTML = "";
         searchResults.forEach(place => {
-            
+            const item = `
+            <div class="search-item">
+                <img src="${place.imageUrl}" style="width:400px;">
+                <h1>${place.name}</h1>
+                <p>${place.description}</p>
+                <button>View</button>
+            </div>
+            `;
+            searchResultContainer.innerHTML += item;
+        });
+    } else if (searchResults && searchTerm === "count") {
+        const searchResultContainer = document.getElementById("search-results");
+        searchResultContainer.innerHTML = "";
+
+        searchResults.forEach((country) => {
+            console.log(country);
+            country.cities.forEach(place => {
+                const item = `
+                    <div class="search-item">
+                        <img src="${place.imageUrl}" style="width:400px;">
+                        <h1>${place.name}</h1>
+                        <p>${place.description}</p>
+                        <button>View</button>
+                    </div>
+                    `;
+                searchResultContainer.innerHTML += item;
+
+            });
+
         });
     }
-  
-    console.log(searchResults);
 }
